a4ba61cbea234f7a484f292eeedc90eb8f7a1c1d
sergey radionov <rsatom@gmail.com>
2024-04-12 00:12:17 +0700

    enableTLS.sh: it's not recommended to use apt in scripts

--- enableTLS.sh
+++ enableTLS.sh
@@ -39,9 +39,9 @@ set -e
 
 sudo snap install --classic certbot
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install nginx -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
 
 sudo systemctl stop nginx
 # sudo rm -rf /etc/nginx/sites-enabled/*
