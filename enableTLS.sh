21fd673af62e523b498de34277cdd98a4b1da1c6
sergey radionov <rsatom@gmail.com>
2024-04-14 15:20:27 +0700

    enableTLS.sh: define timeouts explicitly

--- enableTLS.sh
+++ enableTLS.sh
@@ -82,6 +82,8 @@ server {
   location / {
       proxy_pass http://127.0.0.1:5554/;
       proxy_http_version 1.1;
+      proxy_read_timeout 240s;
+      proxy_send_timeout 240s;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection "upgrade";
       proxy_set_header X-Real-IP \$remote_addr;
