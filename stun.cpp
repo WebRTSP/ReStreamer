0b0410ca197ebf5b17950e9ccbe9d7be3df147e4
sergey radionov <rsatom@gmail.com>
2026-05-17 10:34:08 +0700

    stun: used more reliable way to detect public IP

--- stun.cpp
+++ stun.cpp
@@ -1,6 +1,7 @@
 #include "stun.h"
 
 #include <string>
+#include <future>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,14 +10,34 @@
 #include <gio/gio.h>
 
 #include <stun/usages/bind.h>
+#include <nice/agent.h>
+
+#include <CxxPtr/GlibPtr.h>
+
+#include "RtStreaming/GstRtStreaming/Helpers.h"
 
 #include "Log.h"
 
 
-#define DEFAULT_STUN_SERVICE "3478"
+namespace
+{
 
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(addrinfo, freeaddrinfo)
+struct LibNiceUnref
+{
+    void operator() (NiceAgent* agent)
+        { g_object_unref(agent); }
+};
 
+typedef
+    std::unique_ptr<
+        NiceAgent,
+        LibNiceUnref> NiceAgentPtr;
+
+enum {
+    DEFAULT_STUN_PORT = 3478
+};
+
+}
 
 std::optional<std::string> DetectPublicIP(const WebRTCConfig& webRTCConfig)
 {
@@ -24,12 +45,15 @@ std::optional<std::string> DetectPublicIP(const WebRTCConfig& webRTCConfig)
 
     std::optional<std::string> stunUrl;
 
+    using GstRtStreaming::IceServerType;
+    using GstRtStreaming::ParseIceServerType;
     for(const std::string& iceServer: webRTCConfig.iceServers) {
-        if(0 == iceServer.compare(0, 5, "stun:")) {
+        if(IceServerType::Stun ==  ParseIceServerType(iceServer)) {
             stunUrl = iceServer;
             break;
         }
     }
+
     if(!stunUrl) {
         stunUrl = "stun://stun.l.google.com:19302";
         log->info("There is no STUN server provided in config. Using hardcoded one to detect public IP: {}", *stunUrl);
@@ -44,57 +68,110 @@ std::optional<std::string> DetectPublicIP(const WebRTCConfig& webRTCConfig)
 
     const gchar* scheme = g_uri_get_scheme(uri);
     const gchar* host = g_uri_get_host(uri);
-    gint port = g_uri_get_port(uri);
+    const gint port = g_uri_get_port(uri);
 
-    addrinfo hints {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_DGRAM
-    };
+    std::future<std::pair<std::string, std::string>> future = std::async(
+        std::launch::async,
+        [host, port] () noexcept -> std::pair<std::string, std::string> {
+            GMainContextPtr mainContextPtr(g_main_context_new());
+            GMainContext* mainContext = mainContextPtr.get();
 
-    for(guint i = 0; i < 5; ++i) {
-        if(i != 0) {
-            const guint baseDelay = 1 << std::min(i, 8u);
-            const int delay = g_random_int_range(baseDelay,  baseDelay << 1);
-            log->info("Sleeping for {} seconds before try to get public IP another time...", delay);
-            sleep(delay);
-        }
+            GMainLoopPtr mainLoopPtr(g_main_loop_new(mainContext, FALSE));
+            GMainLoop* mainLoop = mainLoopPtr.get();
 
-        g_autofree gchar* service = (port != -1) ? g_strdup_printf("%i", port) : nullptr;
-        g_autoptr(addrinfo) addrinfos = nullptr;
-        if(int ret = getaddrinfo(host, service ? service : DEFAULT_STUN_SERVICE, &hints, &addrinfos)) {
-            log->error("Failed to resolve \"{}\": {}", host, gai_strerror(ret));
-            continue;
-        }
+            NiceAgentPtr niceAgentPtr(nice_agent_new(mainContext, NICE_COMPATIBILITY_RFC5245));
+            NiceAgent* niceAgent = niceAgentPtr.get();
 
-        addrinfo* ai = addrinfos;
-        sockaddr_storage publicAddressStorage;
-        socklen_t publicAddressStorageLen = sizeof(publicAddressStorage);
+            struct Context {
+                GMainLoop* mainLoop;
+                std::string externalIp;
+                std::string externalIpV6;
+            } context = {
+                .mainLoop = mainLoop
+            };
 
-        const StunUsageBindReturn bindRet =
-            stun_usage_bind_run(
-                ai->ai_addr,
-                ai->ai_addrlen,
-                &publicAddressStorage,
-                &publicAddressStorageLen);
-        if(bindRet != STUN_USAGE_BIND_RETURN_SUCCESS) {
-            log->error("Failed to do STUN bind requst");
-            continue;
-        }
-        assert(publicAddressStorage.ss_family == AF_INET);
+            g_object_set(
+                G_OBJECT(niceAgent),
+                "controlling-mode", TRUE,
+                "stun-server", host,
+                "stun-server-port", port > 0 ? port : DEFAULT_STUN_PORT,
+                nullptr);
 
-        const sockaddr_in& publicAddress = *reinterpret_cast<sockaddr_in*>(&publicAddressStorage);
+            auto newCandidateFull = + [] (
+                NiceAgent* agent,
+                NiceCandidate* candidate,
+                gpointer userData)
+            {
+                Context& context = *static_cast<Context*>(userData);
+                if(candidate->type == NICE_CANDIDATE_TYPE_SERVER_REFLEXIVE) {
+                    gchar address[NICE_ADDRESS_STRING_LEN];
+                    nice_address_to_string(&candidate->addr, address);
+                    switch(candidate->addr.s.addr.sa_family) {
+                    case AF_INET:
+                        context.externalIp = address;
+                        break;
+                    case AF_INET6:
+                        context.externalIpV6 = address;
+                        break;
+                    }
+                }
+            };
+            g_signal_connect(
+                G_OBJECT(niceAgent),
+                "new-candidate-full",  G_CALLBACK(newCandidateFull),
+                &context);
 
-        char publicIp[INET6_ADDRSTRLEN];
-        if(!inet_ntop(publicAddress.sin_family, &publicAddress.sin_addr, publicIp, sizeof(publicIp))) {
-            log->error("Failed to stringize IP address");
-            continue;
-        }
+            auto candidateGatheringDone = + [] (
+                NiceAgent* agent,
+                guint streamId,
+                gpointer userData)
+            {
+                Context& context = *static_cast<Context*>(userData);
+                g_main_loop_quit(context.mainLoop);
+            };
+            g_signal_connect(
+                G_OBJECT(niceAgent),
+                "candidate-gathering-done",  G_CALLBACK(candidateGatheringDone),
+                &context);
 
+            const guint streamId = nice_agent_add_stream(niceAgent, 1);
+            if(streamId == 0)
+                return {};
+
+            auto niceRecv = + [] (
+                NiceAgent* agent,
+                guint streamId,
+                guint componentId,
+                guint len,
+                gchar* buf,
+                gpointer data)
+            {
+            };
+            if(!nice_agent_attach_recv(
+                niceAgent,
+                streamId,
+                1,
+                mainContext,
+                niceRecv,
+                   nullptr))
+            {
+                return {};
+            }
+
+            if(!nice_agent_gather_candidates(niceAgent, streamId))
+                return {};
+
+            g_main_loop_run(mainLoop);
+
+            return std::make_pair(context.externalIp, context.externalIpV6);
+        });
+
+    const std::string publicIp = future.get().first;
+    if(publicIp.empty()) {
+        log->warn("Failed to detect public IP");
+    } else {
         log->info("Detected public IP: {}", publicIp);
-
-        return publicIp;
     }
 
-    log->warn("Failed to detect public IP");
-    return {};
+    return publicIp;
 }
